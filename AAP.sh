9508648d3a4ed5a4ea745c97f86e95246d8c3982
Rhine Nya <108107785+nya-main@users.noreply.github.com>
2024-02-21 17:47:46 +0800

    fix: Parameter -n detection fix

--- AAP.sh
+++ AAP.sh
@@ -128,7 +128,7 @@ get_device_boot
 get_tools
 patch_boot
 if [[ -n "${NOINSTALL}" ]]; then
-	echo "${YE8LLOW}W: The -n parameter was received. Won't flash the boot partition.${RESET}"
+	echo "${YELLOW}W: The -n parameter was received. Won't flash the boot partition.${RESET}"
 	echo "${BLUE}I: Now copying patched image to /storage/emulated/0/patched_boot.img...${RESET}"
 	mv ${WORKDIR}/new-boot.img /storage/emulated/0/patched_boot.img
 	rm -rf ${WORKDIR}
